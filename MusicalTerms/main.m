a7dffdd4a81dba4ccf2beb09fee61e7d09ecefbe
runes <runes@runes-MacBook-Pro.local>
2012-09-29 10:25:10 +0800

    Initial Commit

---
+++ MusicalTerms/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusicalTerms
+//
+//  Created by runes on 12-9-29.
+//  Copyright (c) 2012年 runes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
